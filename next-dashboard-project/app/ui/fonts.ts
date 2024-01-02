2abf4439b3c3147491d147ce4633380dadef50cc
Joao <dev.joaogustavo.dev@gmail.com>
2024-01-02 09:23:57 -0300

    studying next | learn how to add fonts and images preventing layout shifting

---
+++ next-dashboard-project/app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Lusitana, Inter } from "next/font/google";
+
+export const primaryFont = Inter( { subsets: ["latin"]} ); 
+export const secondaryFont = Lusitana( { weight: ["400"], subsets: ["latin"] } );
